08e8001a1ea3fdefb6e33c02d95768c01dfda474
Vinícius <matosvb@gmail.com>
2020-11-23 11:11:10 -0300

    Implementação da criação de colunas oriundas da data, e salvamento do arquivo em CSV.

--- parse_sim_dbf.py
+++ parse_sim_dbf.py
@@ -1,54 +1,74 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 import dbfread
 import datetime
 
 from dateutil.relativedelta import relativedelta
 
 
-class SIMFieldParser(dbfread.FieldParser):
+def parse_date(d):
+    try:
+        year = int(d[4:])
+        month = int(d[2:4])
+        day = int(d[:2])
+    except (TypeError, ValueError):
+        return None
+    return datetime.date(year, month, day)
 
-    def parseC(self, field, data):
-        if field.name.startswith('DT') and field.length == 8:
-            try:
-                year = int(data[4:])
-                month = int(data[2:4])
-                day = int(data[:2])
-            except (TypeError, ValueError):
-                return dbfread.InvalidValue(data)
-            return datetime.date(year, month, day)
-        if field.name == 'IDADE' and field.length == 3:
-            try:
-                unit, value = int(data[:1]), int(data[1:])
-            except (TypeError, ValueError):
-                return dbfread.InvalidValue(data)
-            if unit == 5:
-                unit, value = 4, value + 100
-            args = ['minutes', 'hours', 'days', 'months', 'years']
-            if 0 <= unit < len(args):
-                return relativedelta(**{args[unit]: value})
-            return dbfread.InvalidValue(data)
-        return super().parseC(field, data)
+
+def parse_age(d):
+    try:
+        unit, value = int(d[:1]), int(d[1:])
+    except (TypeError, ValueError):
+        return None
+    if unit == 5:
+        unit, value = 4, value + 100
+    args = ['minutes', 'hours', 'days', 'months', 'years']
+    if 0 <= unit < len(args):
+        return relativedelta(**{args[unit]: value})
+    return None
 
 
 def main():
-    desc = 'This script reads a DBF file containing death causes encoded '  \
-            'according to "Sistema de Informação sobre Mortalidade" (SIM)'
+    desc = '''
+    This script reads a DBF file containing death causes encoded
+    according to "Sistema de Informação sobre Mortalidade" (SIM).
+    It adds a few columns and saves the file as CSV.
+    '''
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('files', nargs='+', type=argparse.FileType('r'),
-                        help='DBF file names')
+    parser.add_argument('input_file', type=argparse.FileType('r'),
+                        help='input file name (DBF)')
+    parser.add_argument('output_file', type=argparse.FileType('w'),
+                        help='output file name (CSV)')
     a = parser.parse_args()
-    n = 0
-    for f in a.files:
-        print('Reading', f.name)
-        with dbfread.DBF(f.name, parserclass=SIMFieldParser) as dbf:
-            for row in dbf:
-                n += 1
-                print('\n' + 70 * '-' + '\n' + 'Linha', n)
-                for k, v in row.items():
-                    print('%20s = %s' % (k, v))
-                input('[Press ENTER to continue]')
+    with dbfread.DBF(a.input_file.name) as reader:
+        all_rows = [row for row in reader]
+        cols = reader.field_names
+    cols = cols[:1] + [
+        'DATA',           # 1..31
+        'SEMANAEPI',      # ?
+        'MES',            # 1..12
+        'ANO',            # YYYY
+        'AREARENDA',      # ?
+        'DISTRITOSAUDE',  # ?
+        'CAPCID',         # ?
+        'CAUSAESP',       # ?
+    ] + cols[1:]
+    with open(a.output_file.name, 'w') as fd:
+        writer = csv.DictWriter(fd, cols, delimiter=',',
+                                quoting=csv.QUOTE_NONNUMERIC)
+        writer.writeheader()
+        for row in all_rows:
+            # parse date and add columns with the extracted data
+            date = parse_date(row['DTOBITO'])
+            row.update({
+                'DATA': date.day,
+                'ANO': date.month,
+                'MES': date.year,
+            })
+            writer.writerow(row)
 
 
 if __name__ == '__main__':
